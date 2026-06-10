c65aab45c819651ec216d3c794edd83c6ca0e0cf
Naw San <nawsanbmo@gmail.com>
2026-06-11 05:00:43 +0900

    initial commit

---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+export default function AboutPage(){
+  return <div>
+    <h1>My about page</h1>
+  </div>
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Navigation from "../components/navigation";
+
+export default function CabinPage() {
+  return (
+    <div>
+      <Navigation />
+      <h1>cabin page</h1>
+    </div>
+  );
+}
