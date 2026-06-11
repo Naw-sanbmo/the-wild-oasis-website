c5e4915b95151a26a7aab8fe220b5adc0b4afc5f
Naw San <nawsanbmo@gmail.com>
2026-06-12 05:01:39 +0900

    add layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,10 @@
-export default function AboutPage(){
-  return <div>
-    <h1>My about page</h1>
-  </div>
+import Navigation from "../components/navigation";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Navigation />
+      <h1>My about page</h1>
+    </div>
+  );
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,6 +1,9 @@
+import Navigation from "../components/navigation";
+
 export default function AccountPage() {
   return (
     <div>
+      <Navigation />
       <h1>My account Page</h1>
     </div>
   );
