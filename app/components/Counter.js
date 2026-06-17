ba86741a485565706c0030e60a68fd414a2c6b51
Naw San <nawsanbmo@gmail.com>
2026-06-18 03:56:54 +0900

    organize folders and files structure

--- app/components/Counter.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useState } from "react";
-
-export default function Counter() {
-  const [count, setCounter] = useState(0);
-  return <button onClick={() => setCounter((c) => c + 1)}>{count}</button>;
-}
