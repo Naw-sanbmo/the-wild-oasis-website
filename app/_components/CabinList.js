0100a671a7b5ad8952ad714e4a845d38355b0f18
Naw San <nawsanbmo@gmail.com>
2026-07-10 04:16:43 +0900

    add web api URLSearchParmas for URL query parameters manipulation

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -12,10 +12,10 @@ async function CabinList({ filter }) {
   let displayedCabins;
   if (filter === "all") displayedCabins = cabins;
   if (filter === "small")
-    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 4);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
   if (filter === "medium")
     displayedCabins = cabins.filter(
-      (cabin) => cabin.maxCapacity >= 5 && cabin.maxCapacity <= 7,
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7,
     );
   if (filter === "large")
     displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
