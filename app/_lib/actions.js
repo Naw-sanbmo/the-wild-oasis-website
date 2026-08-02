d551fe03c51d21cbe042516eccbde00c18918b03
Naw San <nawsanbmo@gmail.com>
2026-08-03 03:51:50 +0900

    add updating reservations

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import supabase from "./supabase";
+import { redirect } from "next/navigation";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -61,4 +63,44 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get("bookingId"));
+
+  // 1) AUTHENTICATION
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+  console.log(formData);
+
+  // 2) AUTHORIZATION
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to UPDATE this booking");
+
+  // 3) BUILDING UPDATE DATA
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // 4) MUTATION
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5) ERROR HANDLING
+  if (error) throw new Error("Booking could not be updated");
+
+  // 6) REVALIDATION
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  // 7) REDIRATING
+  redirect("/account/reservations");
 }
